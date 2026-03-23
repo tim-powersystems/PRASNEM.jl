2ed8670821d370b1cf3a3f11dc5af8d6e75c888b
Tim Kopka <tkopka@student.unimelb.edu.au>
2026-03-23 11:51:32 +1100

    Add function to update generator commitment only if actually ramping limited

--- src/updating/updateGeneratorOperation.jl
+++ src/updating/updateGeneratorOperation.jl
@@ -2,11 +2,21 @@
     updateUnitCommitment!(sys, res)
 
 Update the generator capacities based on commitment status (using 'res.gon').
+
+The flag consider_ramping determines whether to consider generators that are not ramping limited as always available. Else their availability might be reduced to zero in timesteps when they are not committed/producing.
 """
-function updateUnitCommitment!(sys, res)
+function updateUnitCommitment!(sys, res; consider_ramping=true)
 
     # Update generator capacities based on commitment status
-    sys.generators.capacity .= sys.generators.capacity .* res.gon
+
+    if consider_ramping
+        not_ramping_limited = zeros(size(res.gon))
+        not_ramping_limited[findall(res.p_gen_max .== sys.generators.capacity)] .= 1
+        sys.generators.capacity .= sys.generators.capacity .* max.(res.gon, not_ramping_limited)
+    else
+        
+        sys.generators.capacity .= sys.generators.capacity .* res.gon
+    end
 
     return sys
 end
