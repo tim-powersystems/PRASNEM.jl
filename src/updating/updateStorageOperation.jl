d9cd08ee5cf981036954d8c6da4dc5846ca5541d
Tim Kopka <tim.kopka@student.unimelb.edu.au>
2026-05-13 13:14:31 +1000

    Update energy derating function

--- src/updating/updateStorageOperation.jl
+++ src/updating/updateStorageOperation.jl
@@ -14,18 +14,30 @@ This function is derating short-term energy storage capacities based on a provid
 The derating_mapping is a Dict where keys are energy storage duration thresholds (in hours) and values are the derating factors (between 0 and 1).
 
 """
-function updateEnergyDerating!(sys; derating_mapping = Dict(1.5 => 0.5, 3.5 => 0.75, 7.5 => 0.9))
+function updateEnergyDerating!(sys; derating_mapping = Dict(1.5 => 0.5, 3.5 => 0.75, 7.5 => 0.9), derate_VPPs=0.5)
+    if any(sys.storages.categories .== "VPP") && derate_VPPs < 1.0
+        @info "VPPs are present in the system and will be derated by a factor of $(derate_VPPs * 100)% regardless of their energy duration."
+    end
 
     lower_bound_hours  = 0.0
     for (derating_hours, derating_factor) in sort(derating_mapping)
         @info("<" * string(derating_hours) * " hours energy storage derated to " * string(derating_factor * 100) * "% capacity.")
         for s in 1:length(sys.storages.names)
+            
             ecap = maximum(sys.storages.energy_capacity[s, :])
             pcap = maximum(sys.storages.discharge_capacity[s, :])  # Assuming capacity is constant over time
-            energy_hours = ecap / pcap
-            if energy_hours > lower_bound_hours && energy_hours <= derating_hours
-                sys.storages.energy_capacity[s, :] .= round.(Int, ecap * derating_factor)
+
+            if (sys.storages.categories[s] == "VPP") && (derate_VPPs < 1.0)
+                # Derate VPPs by the specified factor regardless of their energy duration
+                sys.storages.energy_capacity[s, :] .= round.(Int, sys.storages.energy_capacity[s, :] * derate_VPPs)
+            else
+                # Derate all other storages based on their energy duration
+                energy_hours = ecap / pcap
+                if energy_hours >= lower_bound_hours && energy_hours < derating_hours
+                    sys.storages.energy_capacity[s, :] .= round.(Int, sys.storages.energy_capacity[s, :] * derating_factor)
+                end
             end
+
         end
         lower_bound_hours = derating_hours
     end
